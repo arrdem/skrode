6a45af0041099b0c4d093b9054a76bfc0364c76e
Reid 'arrdem' McKenzie <me@arrdem.com>
2017-08-07 01:45:24 -0700

    Add this for shits and giggles

---
+++ graph_bbdb_schema.py
@@ -0,0 +1,14 @@
+"""
+A really stupid script to visualize the bbdb schema
+"""
+
+from bbdb.schema import Base
+
+from sqlalchemy_schemadisplay import create_schema_graph
+
+graph = create_schema_graph(
+  metadata=Base.metadata,
+  rankdir='LR', # From left to right (instead of top to bottom)
+  concentrate=False # Don't try to join the relation lines together
+)
+graph.write_png('dbschema.png') # write out the file
