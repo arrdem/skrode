e2d78f384d5caf37c8ebde373a6e911555351cc1
Reid 'arrdem' McKenzie <me@arrdem.com>
2017-08-31 17:06:42 -0700

    Linting

--- crawl_keybase.py
+++ crawl_keybase.py
@@ -2,12 +2,8 @@
 Join Twitter handles against the Keybase API.
 """
 
-import sys
-
 from keybase import Api
 from bbdb import schema, config, keybase, make_session_factory
-
-import arrow
 
 
 factory = make_session_factory()
@@ -17,7 +13,7 @@ bbdb_config = config.BBDBConfig()
 if __name__ == '__main__':
   session = factory()
   kb_api = Api()
-  
+
   try:
     keybase.link_keybases(session, kb=kb_api)
 
