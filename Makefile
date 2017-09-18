c9ab616fe3068a8396fdf0f067f35a565d694c62
Reid 'arrdem' McKenzie <me@arrdem.com>
2017-09-18 00:51:53 -0700

    Add some db schema automation

---
+++ Makefile
@@ -0,0 +1,2 @@
+etc/dbschema.png:
+	python ./graph_bbdb_schema.png -o etc/dbschema.png
--- graph_bbdb_schema.py
+++ graph_bbdb_schema.py
@@ -2,13 +2,23 @@
 A really stupid script to visualize the bbdb schema
 """
 
-from bbdb.schema import Base
+import argparse
+import sys
 
 from sqlalchemy_schemadisplay import create_schema_graph
 
-graph = create_schema_graph(
-  metadata=Base.metadata,
-  rankdir="LR", # From left to right (instead of top to bottom)
-  concentrate=False # Don't try to join the relation lines together
-)
-graph.write_png("dbschema.png") # write out the file
+from bbdb.schema import Base
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-o", dest="outfile")
+
+def main(opts):
+  graph = create_schema_graph(
+    metadata=Base.metadata,
+    rankdir="LR", # From left to right (instead of top to bottom)
+    concentrate=False # Don't try to join the relation lines together
+  )
+  graph.write_png(opts.outfile) # write out the file
+
+if __name__ == "__main__" or 1:
+  main(parser.parse_args(sys.argv[1:]))
