f155a65fd8b8a29956b98992cb5336f0bed704eb
Reid 'arrdem' McKenzie <me@arrdem.com>
2017-09-18 01:01:09 -0700

    Overhaul merge in light of various other changes

--- merge.py
+++ merge.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python3
 """
-MERGE. A quick script for merging users.
+MERGE. A quick script for merging personas together.
 """
 
 import argparse
 import sys
 
-from bbdb import session, personas, schema
-
+from bbdb import personas, schema
+from bbdb.config import BBDBConfig
 
 args = argparse.ArgumentParser()
+args.add_argument("-c", "--config",
+                  dest="config",
+                  default="config.yml")
+
 args.add_argument("l")
 args.add_argument("r")
 
-if __name__ == "__main__":
-  opts = args.parse_args(sys.argv[1:])
-  session = session()
+
+def main(opts):
+  config = BBDBConfig(config=opts.config)
+  session = config.get("sql")
 
   l = session.query(schema.Persona).filter_by(id=opts.l).one()
   r = session.query(schema.Persona).filter_by(id=opts.r).one()
 
   personas.merge_left(session, l, r)
-  session.delete(r)
   session.commit()
+
+
+if __name__ == "__main__":
+  main(args.parse_args(sys.argv[1:]))
