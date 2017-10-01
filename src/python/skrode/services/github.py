de4eab8fd11dbce0b98f5f0e0962fcffc6e3318e
Reid 'arrdem' McKenzie <me@arrdem.com>
2017-10-01 11:21:35 -0700

    Strip down the github service def

--- src/python/skrode/services/github.py
+++ src/python/skrode/services/github.py
@@ -6,16 +6,12 @@ from __future__ import absolute_import
 
 import re
 
-from skrode.personas import merge_left
-from skrode.schema import Account, AccountRelationship, Human, Name, Persona, Service, get_or_create
-from skrode.services import mk_insert_user, mk_service
-
-from arrow import utcnow as now
+from skrode.services import mk_insert_user as _mk_insert_user, mk_service as _mk_service
 
 
-insert_github = mk_service("Github", ["http://github.com",
-                                      "http://gist.github.io",
-                                      "http://github.io"])
+insert_github = _mk_service("Github", ["http://github.com",
+                                       "http://gist.github.io",
+                                       "http://github.io"])
 
 _gh_user_pattern = re.compile(r"(https?://)?((gist\.)?github\.(io|com))/(?P<username>[^/?]+)(/.+)?(&.+)?")
 
@@ -28,4 +24,4 @@ def external_id(username):
   return "github+user:%s" % username
 
 
-insert_user = mk_insert_user(insert_github, external_id)
+insert_user = _mk_insert_user(insert_github, external_id)
