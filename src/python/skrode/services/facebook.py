572be10ebb9bd6a2ee28c950ebf267317a918c5f
Reid 'arrdem' McKenzie <me@arrdem.com>
2017-10-01 11:20:56 -0700

    Remove false dependency

--- src/python/skrode/services/facebook.py
+++ src/python/skrode/services/facebook.py
@@ -6,16 +6,12 @@ from __future__ import absolute_import
 
 import re
 
-from skrode.personas import merge_left
-from skrode.schema import Account, AccountRelationship, Human, Name, Persona, Service, get_or_create
 from skrode.services import mk_insert_user, mk_service
-
-from arrow import utcnow as now
 
 
 insert_facebook = mk_service("Facebook", ["http://facebook.com", "http://messenger.com"])
-
 _fb_user_pattern = re.compile(r"(https?://)?(facebook.com)/(?P<username>[^/?]+)(/.+)?(&.+)?")
+
 
 def external_id(username):
   m = re.match(_fb_user_pattern, username)
