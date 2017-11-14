6652dc29caeec7a1795afaabca6353be5cca3605
Reid 'arrdem' McKenzie <me@arrdem.com>
2017-11-14 01:50:37 -0800

    Get initial IMAP email fetching / plaintext normalization working

--- scripts/imap/imap.py
+++ scripts/imap/imap.py
@@ -4,15 +4,15 @@ Ingest mail from one or more configured IMAP accounts.
 
 from __future__ import print_function
 
-import StringIO
 from traceback import format_exc
 import argparse
 import logging as log
-from email.message import Message
 import sys
 
 from skrode.config import Config
 from skrode.imap import IMAPWrapper
+
+import html2text
 
 args = argparse.ArgumentParser()
 args.add_argument("-c", "--config",
@@ -20,6 +20,46 @@ args.add_argument("-c", "--config",
                   default="config.yml")
 args.add_argument("-a", "--account",
                   dest="account")
+
+
+def message_parts_and_types(message):
+  """Converts a MIME (potentially multipart) message to a sequence of pairs (type, \ -> str)
+
+  The functions are 0-argument delays returning the decoded text of message parts.
+  """
+
+  def normalize(text):
+    return text.decode("utf-8").replace(u"\xa0", " ")  # Fuck your non-breaking spaces
+
+  if message.is_multipart():
+    for part in message.walk():
+      yield part.get_content_type(), lambda: normalize(part.get_payload(decode=True))
+
+  else:
+    yield message.get_content_type(), lambda: normalize(message.get_payload(decode=True))
+
+
+def message_to_text(message):
+  """Does its damndest to convert a message to plain text.
+
+  So far I've only really seen two "kinds" of email - text/plain encoded mail, and text/html email
+  with a bunch more stuff to it. This function attempts to normalize to text/plain, using html2text
+  to flatten the HTML to "plain" text if a text/plain rendering isn't available.
+
+  Returns None of it can't find a "normalization" of the email.
+  """
+
+  for type, part in message_parts_and_types(message):
+    if type == "text/plain":
+      return part()
+
+  h = html2text.HTML2Text()
+  h.ignore_links = True
+  h.bypass_tables = True
+
+  for type, part in message_parts_and_types(message):
+    if type == "text/html":
+      return h.handle(part())
 
 
 def main(opts):
@@ -37,15 +77,19 @@ def main(opts):
       with folder:
         message_ids = list(folder.search(None, 'ALL'))
 
+        example = []
         # Find the shortest example message from each folder
-        _blob = None
         for message_id in message_ids:
-          err, data = imap_server.fetch(message_id, '(BODY[HEADER.FIELDS (MESSAGE-ID)])')
-          blob = 'Message %s\n%s\n%s\n' % (message_id, err, data)
-          if not _blob or len(_blob) > len(blob):
-            _blob = blob
+          data = imap_server.fetch(message_id, '(BODY[HEADER.FIELDS (MESSAGE-ID)] BODY[])')
 
-        log.info(_blob)
+          if sys.getsizeof(repr(data)) < sys.getsizeof(repr(example)):
+            example = data
+          elif not example:
+            example = data
+
+        log.info("%s", example)
+        sys.exit(0)
+
   finally:
     if imap_server:
       imap_server.logout()
--- src/python/skrode/imap/__init__.py
+++ src/python/skrode/imap/__init__.py
@@ -1,10 +1,8 @@
 """
-A wrapper around imaplib.* for convenience.
-
-Primarily written because imapclient is crap (under-documented/under-exampled) and I'm still
-learning the mechanics of the protocol so no better time to write an excessively thin wrapper.
+An imap driver that actually makes some attempt to provide a structured access model.
 """
 
+from email import message_from_string
 import logging as log
 import re
 
@@ -27,6 +25,7 @@ FOLDER_STATUS_K_V_PATTERN = re.compile(
   r'(?P<condition>\w+) (?P<value>\d+)'
 )
 
+K_PATTERN = re.compile(r"(?P<msg_id>\d+)?\s\(?(?P<data_item>.*) \{\d+\}")
 
 def _ensure_quoted(text):
   if not re.match(QUOTED_PATTERN, text):
@@ -39,14 +38,24 @@ class IMAPException(Exception):
   pass
 
 
+class IMAPMessage(object):
+  def __init__(self, client, folder):
+    self._client = client
+    self._folder = folder
+
+  def fetch(self, items):
+    pass
+
+
 class IMAPFolder(object):
   def __init__(self, client, name, flags, delimeter, status=None):
-    self.name = name
-    self.flags = flags
-    self.delimeter = delimeter
     self._client = client
     self._recursion = 0
     self._status = None
+
+    self.name = name
+    self.flags = flags
+    self.delimeter = delimeter
 
   def __repr__(self):
     return "<IMAPFolder {0.name!r} status={0._status}>".format(self)
@@ -56,10 +65,10 @@ class IMAPFolder(object):
     self._recursion += 1
 
   def __exit__(self, *args, **kwargs):
-    if self._recursion == 1:
+    self._recursion = max(0, self._recursion - 1)
+
+    if self._recursion == 0:
       self._client.close()
-    else:
-      self._recursion -= 1
 
   def search(self, *args, **kwargs):
     with self:
@@ -67,6 +76,10 @@ class IMAPFolder(object):
 
   def status(self):
     return self._client.status(self.name)
+
+  def fetch(self, *args, **kwargs):
+    with self:
+      return self._client.fetch(*args, **kwargs)
 
 
 class IMAPFolderStatus(object):
@@ -194,16 +207,103 @@ class IMAPWrapper(object):
     else:
       raise IMAPException(results)
 
-  def fetch(self, *args, **kwargs):
-    """
-    Fetch messages.
+  def fetch(self, sequence_set, data_items):
+    """Fetch messages.
+
+    https://tools.ietf.org/html/rfc3501#section-6.4.5
+
+    Accepts a "sequence set", being either the ID, ID range(s) or UUID of a message in the current
+    mailbox, and a string being either an atom naming a datom to fetch, or a parenthesized list of
+    datoms to fetch or a macro representing a list of datoms.
+
+    Returns a map from message IDs (as strings) to maps of datom (as named by the server) to values
+    as returned by the server.
+
+    .. code-block:: python
+
+       >>> with inbox:
+       ...   server.fetch(18, '(BODY[HEADER.FIELDS (MESSAGE-ID)] BODY[])')
+       ...
+       {'18': {'BODY[HEADER.FIELDS (MESSAGE-ID)]': 'Message-ID: <....>\r\n\r\n',
+               'BODY[]': '....\r\n\r\n'}}
     """
 
-    err, results = self._client.fetch(*args, **kwargs)
+    def fake_defaultdict(m, k):
+      if k not in m:
+        m[k] = {}
+      return m[k]
+
+    def parse_multi_kv_response(result_tuples):
+      """Implementation detail.
+
+      RFC350 6.4.5. defines the FETCH command to return a sequence of untagged (* prefixed)
+      responses, of the form `* <msgid> FETCH ....`, followed by a tagged terminating response ala
+      `OK ...`. Each individual `FETCH` response is itself is an ordered association list of datom
+      name to data, for each datom requested from the server in the request's data items.
+
+      The Python API driver can sort of figure out the association list pairs, but doesn't do a good
+      job of it. What the user gets back is a sequence of (k, v) tuples and the string ")". The
+      first k has as a prefix the ID of the message. The ")" denotes the end of a single email's
+      sequence of k/v pairs. For instance...
+
+      .. code-block:: python
+
+         >>> server.fetch(18, '(BODY[HEADER.FIELDS (MESSAGE-ID)] BODY[])')
+         [('18 (BODY[HEADER.FIELDS (MESSAGE-ID)] {84}',
+           'Message-ID: <....>\r\n\r\n'),
+          (' BODY[] {4690}',
+           '....\r\n'),
+          ')']
+
+      This data structure isn't completely wrong, but it also isn't directly useful at all.
+
+      This function eats the incompletely processed sequence of k/vs and delimiters, returning a map
+      of message IDs to response k/vs.
+      """
+
+      result = {}
+      message_id = None
+
+      for line in result_tuples:
+        if line == ")":
+          continue
+        else:
+          k, v = line
+          m = re.match(K_PATTERN, k)
+          message_id = m.group("msg_id") or message_id
+          fake_defaultdict(result, message_id)[m.group("data_item")] = v
+
+      return result
+
+    err, results = self._client.fetch(sequence_set, data_items)
     if err == "OK":
-      return results
+      # At this point results is a rather complicated thing.
+      return parse_multi_kv_response(results)
     else:
       raise IMAPException(results)
+
+  def fetch_as_emails(self, sequence_set):
+    """
+    Fetches a sequence set of emails as email.message.Message objects.
+
+    Returns a map of message ID to Message object.
+    """
+
+    err, results = self._client.fetch(sequence_set, "(BODY[])")
+    if err != "OK":
+      raise IMAPException(results)
+
+    actual_results = {}
+
+    for line in results:
+      if line == ")":
+        continue
+
+      k, v = line
+      m = re.match(K_PATTERN, k)
+      actual_results[m.group("msg_id")] = message_from_string(v)
+
+    return actual_results
 
   def list(self, *args, **kwargs):
     """
